87aa4035cbb71b9ecceda3860bba870bba847644
Enrico Messall <enrico.messall@arematics.com>
2021-05-09 22:14:07 +0200

    Menu Update + Angular Material + Font Awesome  + Added Angular Material  + Added Font Awesome  + Created group component for menu  + Some style update

--- awiki-frontend/src/app/components/menu/menu.component.ts
+++ awiki-frontend/src/app/components/menu/menu.component.ts
@@ -14,15 +14,16 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const entries: WikiEntry[] = [{groupId: 1, id: 1, orderIndex: 0, title: 'Kekw'}, {groupId: 1, id: 2, orderIndex: 1, title: 'Kek2'}];
+    const entries: WikiEntry[] = [{groupId: 1, id: 1, orderIndex: 0, title: 'TestEntry 1'},
+      {groupId: 1, id: 2, orderIndex: 1, title: 'TestEntry 2'}];
     this.groups = [{id: 1, title: '', orderIndex: 0, entries},
-      {id: 2, title: 'Vallah du hübsche', orderIndex: 0, entries},
-      {id: 3, title: 'asf du hübsche', orderIndex: 0, entries},
-      {id: 4, title: 'fhdfh', orderIndex: 0, entries},
-      {id: 5, title: 'ASF', orderIndex: 0, entries},
-      {id: 6, title: 'vxcFG', orderIndex: 0, entries},
-      {id: 7, title: 'WRSDG', orderIndex: 0, entries},
-      {id: 8, title: 's', orderIndex: 0, entries}];
+      {id: 2, title: 'Test 1', orderIndex: 0, entries},
+      {id: 3, title: 'Test 2', orderIndex: 0, entries},
+      {id: 4, title: 'Test 3', orderIndex: 0, entries},
+      {id: 5, title: 'Test 4', orderIndex: 0, entries},
+      {id: 6, title: 'Test 5', orderIndex: 0, entries},
+      {id: 7, title: 'Test 6', orderIndex: 0, entries},
+      {id: 8, title: 'Test 7', orderIndex: 0, entries}];
   }
 
 }
